40683abee40927638a284857ac9e39a366facea6
Anshika Pandey <anshipandey6565@gmail.com>
2026-04-16 23:36:18 +0530

    receive api

--- src/app.js
+++ src/app.js
@@ -13,6 +13,7 @@ app.use(cookieParser());
 app.use("/",authRouter);
 app.use("/",profileRouter);
 app.use("/",requestRouter);
+app.use("/",userRouter);
 connectDB().then(()=>{
     console.log("db connected");
     app.listen(3000,()=>{
---
+++ src/routes/user.js
@@ -0,0 +1,47 @@
+const express=require('express');
+const userRouter=express.Router();
+const {userAuth}=require("../middlewares/auth");
+const ConnectionRequest=require("../models/connectionRequest");
+const USER_SAFE="firstName lastName photoUrl age gender about skills";
+userRouter.get("/user/requests/received",userAuth,async (req,res)=>{
+    try{
+        const loggedInUser=req.user;
+        const connectionRequests=await ConectionRequest.find({
+            toUserId:loggedInUser._id,
+            status:"interested",
+            
+
+        }).populate("fromUserId",USER_SAFE);
+
+        res.json({message:"data received successfully",data:connectionRequests});
+
+    }
+    catch(err){
+        throw new Error("ERROR");
+    }
+
+});
+userRouter.get("/user/connections",userAuth,async (req,res)=>{
+    try{
+        const loggedInUser=req.user;
+        const connectionRequests=await ConnectionRequest.find({
+            $or:[
+                {toUserId:loggedInUser._id,status:"accepted"},
+                {fromUserId:loggedInUser._id,status:"accepted"},
+
+            ],
+        }).populate("fromUserId",USER_SAFE).populate("toUserId",USER_SAFE);
+        const data=connectionRequests.map((row)=>{
+            if(row.fromUserId._id.toString()==loggedInUser._id){
+                return row.toUserId;
+            }
+            return row.fromUSerId;
+        });
+        res.json({data});
+
+    }
+    catch(err){
+        throw new Error("ERROR");
+    }
+})
+module.exports={userRouter};
